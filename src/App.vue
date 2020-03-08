2a2ab87585ec7dbed8f80280b32274e5d90eabdb
Jae <jae@Jaes-MacBook-Pro.local>
2020-03-08 19:09:17 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container flex h-screen text-center overflow-hidden">
+    <div
+      class="m-6 md:rounded-lg bg-white m-auto max-w-sm shadow-2xl sm:rounded-lg"
+    >
+      <div class="sm:flex sm:items-center px-6 py-4 items-center text-center">
+        <div class="text-center sm:text-left sm:flex-grow">
+          <div class="mb-4 text-center">
+            <h1 class="text-2xl leading-tight">Clima</h1>
+            <p class="text-sm leading-tight text-grey-dark">The Simple Weather App Made With &hearts; By <a class="text-purple-600" href="https://github.com/jaetooledev">@jaetooledev</a>.</p>
+            <hr class="my-2">
+            <input type="text" class="my-2 w-full shadow py-1 px-2 text-small" name="location" placeholder="Enter Your Location" id="address" v-model="location" v-debounce:300ms="getTemprature">
+            <div v-if="response">
+              <hr class="my-2">
+              <h2 class="text-lg">It's Currently {{currentTemprature}}&deg;C!</h2>
+              <p class="text-sm" v-if="currentTemprature < 10">It's a bit cold outside currently!</p>
+              <p class="text-sm" v-if="currentTemprature >= 10">It's pretty nice outside currently!</p>
+            </div>
+          </div>
+          <!-- <div class="flex flex-wrap">
+            <button
+              class="text-xs font-semibold rounded-full px-4 py-1 mx-3 leading-normal bg-white border border-blue text-blue hover:bg-blue hover:text-white"
+            >Call</button>
+            <button
+              class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white"
+            >Message</button>
+          </div> -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      location: '',
+      response: null,
+    };
+  },
+  mounted() {
+    require("dotenv").config();
+  },
+  computed: {
+    currentTemprature() {
+      return (this.response.main.temp - 273.15).toFixed(0);
+    }
+  },
+  methods: {
+    getTemprature() {
+      axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + this.location + "&appid=" + process.env.VUE_APP_API_KEY)
+        .then((response) => {
+          this.response = response.data
+        });
+    }
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueDebounce from "vue-debounce";
+
+import "@/assets/css/tailwind.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueDebounce)
 
 new Vue({
   render: h => h(App)
